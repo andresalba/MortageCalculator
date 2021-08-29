0c30c1cefdf7108ce9838c12d144e311b884b9af
Andres Alba <tensinkusum@hotmail.com>
2021-08-29 17:51:44 -0500

    mandatory field implemented

--- Scripts.js
+++ Scripts.js
@@ -2,6 +2,11 @@ var slider1 = document.getElementById("slider1");
 var output1 = document.getElementById("value1");
 var years;
 var interest;
+
+document.getElementById("mf1").style.display = "none";
+document.getElementById("mf2").style.display = "none";
+document.getElementById("mf3").style.display = "none";
+
 output1.innerHTML = slider1.value;
 slider1.oninput = function(){
     output1.innerHTML = this.value;
@@ -27,18 +32,38 @@ slider2.addEventListener("mousemove", function(){
 })
 
 function calculate(){
-    //principle interest calculation
+    //capture data
     let loan = document.getElementById("loanInput").value;
+    let annualTax = document.getElementById("taxInput").value;
+    let annualInsur = document.getElementById("insuranceInput").value;
+    //principle interest calculation
     let calcPi = ((interest/100)/12)*loan/(1-Math.pow((1+((interest/100)/12)), -years*12));
     document.getElementById("prinOutput").innerHTML = calcPi.toFixed(2);
     //tax calculation
-    let annualTax = document.getElementById("taxInput").value;
     let calcTax = annualTax / 12;
     document.getElementById("taxOutput").innerHTML = calcTax.toFixed(2);
     //insurance calculation
-    let annualInsur = document.getElementById("insuranceInput").value;
     let calcInsur = annualInsur / 12;
     document.getElementById("insuranceOutput").innerHTML = calcInsur.toFixed(2);
     //monthly payment calculation
     document.getElementById("monthlyOutput").innerHTML = (calcPi+calcTax+calcInsur).toFixed(2);
+    //error if empthy
+    if(loan > 0){ 
+        document.getElementById("loanInput").style.borderColor = "#cccccc"; 
+        document.getElementById("mf1").style.display = "none";}
+    else{ 
+        document.getElementById("loanInput").style.borderColor = "red";
+        document.getElementById("mf1").style.display = "block";}
+    if(annualTax > 0){
+        document.getElementById("taxInput").style.borderColor = "#cccccc";
+        document.getElementById("mf2").style.display = "none";}        
+    else{ 
+        document.getElementById("taxInput").style.borderColor = "red";
+        document.getElementById("mf2").style.display = "block";}         
+    if(annualInsur > 0){ 
+        document.getElementById("insuranceInput").style.borderColor = "#cccccc"; 
+        document.getElementById("mf3").style.display = "none";}
+    else{ 
+        document.getElementById("insuranceInput").style.borderColor = "red"; 
+        document.getElementById("mf3").style.display = "block";}    
 }
